0c6b1b788b7bd3c9014c9aa284234d06a5c9af28
Govi Santra <156223774+govisantra@users.noreply.github.com>
2025-09-24 11:38:39 +0530

    Update page.tsx

--- src/app/blog-detail/[id]/page.tsx
+++ src/app/blog-detail/[id]/page.tsx
@@ -1,7 +1,15 @@
-export default async function Page({ params }: any) {
+import type { Metadata } from 'next';
+
+interface PageProps {
+  params: { id: string }
+}
+
+const Page = async ({ params }: PageProps) => {
   return (
     <div>
-      <h1>Page for ID: {params.id}</h1>
+      <h1>Single Property ID: {params.id}</h1>
     </div>
-  )
+  );
 }
+
+export default Page;
